68f541edfb56de5aff121f3d0ce2ac0d2bf77617
ankitzin <ankit.zin279@gmail.com>
2023-08-05 13:53:13 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React from Docker and Flask
         </a>
       </header>
     </div>
